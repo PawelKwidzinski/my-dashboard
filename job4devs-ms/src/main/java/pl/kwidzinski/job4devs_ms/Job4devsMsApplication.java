50460bead95de2795464df2194686508a8daa361
pawel <kwidzinski.pawel@gmail.com>
2024-06-03 09:42:25 +0200

    Initial commit

---
+++ job4devs-ms/src/main/java/pl/kwidzinski/job4devs_ms/Job4devsMsApplication.java
@@ -0,0 +1,13 @@
+package pl.kwidzinski.job4devs_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Job4devsMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Job4devsMsApplication.class, args);
+	}
+
+}
---
+++ job4devs-ms/src/test/java/pl/kwidzinski/job4devs_ms/Job4devsMsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kwidzinski.job4devs_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Job4devsMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
