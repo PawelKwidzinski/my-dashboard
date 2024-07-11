8f38717f701c3faddc1d7ab23835793446e51677
pawel <kwidzinski.pawel@gmail.com>
2024-07-11 12:21:21 +0200

    Initial commit

---
+++ healthy-food-ms/src/main/java/pl/kwidzinski/healthy_food_ms/HealthyFoodMsApplication.java
@@ -0,0 +1,13 @@
+package pl.kwidzinski.healthy_food_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthyFoodMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthyFoodMsApplication.class, args);
+	}
+
+}
---
+++ healthy-food-ms/src/test/java/pl/kwidzinski/healthy_food_ms/HealthyFoodMsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kwidzinski.healthy_food_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthyFoodMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
